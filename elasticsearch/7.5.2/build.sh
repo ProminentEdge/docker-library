7ba3246241dfccd1df239269fda7c295f818c88d
srflaxu40 <knepperjm@gmail.com>
2020-04-23 14:15:14 -0500

    updates

---
+++ elasticsearch/7.5.2/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source .env
+
+sed -i '' "s^admin_token^${ADMIN_TOKEN}^" ror.yml
+sed -i '' "s^auth_token^${AUTH_TOKEN}^" ror.yml
+sed -i '' "s^ingest_token^${INGEST_TOKEN}^" ror.yml
+sed -i '' "s^signature_token^${SIGNATURE_TOKEN}^" jwt.yml
+
